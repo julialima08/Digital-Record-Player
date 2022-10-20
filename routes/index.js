65ab42764d27cc5af439376c82795e2895b5939d
Julia Lima <julia.manvailer17@icloud.com>
2022-10-20 17:29:06 -0400

    created index in routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,15 @@
 const { Router } = require('express')
 const router = Router()
 const controllers = require('../controllers')
+
+router.get('/allSongs', controllers.getAllSongs)
+router.get('/song/:id', controller.getSongDetails)
+router.get('/genres/:id', controllers.getGenres)
+router.get('/playlists', controllers.getAllPlaylists)
+router.get('/playlists/:id', controllers.getOnePlaylist)
+router.put('playlists/:id', controllers.updatePlaylist)
+router.delete('/playlist/:id', controllers.deletePlaylist)
+router.delete('/playlists/:id/song/:id', controllers.deleteSongInPlaylist)
+router.post('/addSong', controllers.addSongs)
+
+module.exports = router
