173d73b488a08dc7b970009f35a0f0e02165fdb1
Julia Lima <julia.manvailer17@icloud.com>
2022-10-28 09:55:01 -0400

    deployed

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const app = express()
 app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
-app.use(express.static(`${Digital - Record - Player}/client/build`))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/*', (req, res) => {
-  res.sendFile(`${Digital - Record - Player}/client/build/index.html`)
+  res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
