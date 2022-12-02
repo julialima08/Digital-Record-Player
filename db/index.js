f35ed6079baf507622fca18124e4132b482d39d4
Julia Lima <julia.manvailer17@icloud.com>
2022-12-02 16:34:36 -0500

    deployment changed

--- db/index.js
+++ db/index.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/digitalRecordPlayer'
-
 mongoose
-  .connect(MONGODB_URI)
-  .then(() => {
-    console.log('Successfully connected to MongoDB')
-  })
-  .catch((e) => {
-    console.error('Connection error', e.message)
-  })
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Successfully connected to MongoDB.')
+    })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
 
 const db = mongoose.connection
 
