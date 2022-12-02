aa8789c48eae0eba0948a75fa61de9fc13cf993e
Julia Lima <julia.manvailer17@icloud.com>
2022-12-02 16:50:57 -0500

    .

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
+app.use(express.urlencoded({ extented: false }))
 app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', routes)
